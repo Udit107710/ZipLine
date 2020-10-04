b5c2aa82b2198f9aabe33a97ce9b8d7b3bcd53a2
Udit <uditchaudhary799@gmail.com>
2020-10-04 14:14:01 +0530

    Connection established

--- api.py
+++ api.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_pymongo import pymongo
 
 application = Flask(__name__)
 api = Api(application)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+DB_CONNECTIONG_STRING = "mongodb+srv://app:uBtU55fMt0KLebrl@cluster0.afwwy.gcp.mongodb.net/greendeck?retryWrites=true&w=majority"
+client = pymongo.MongoClient(DB_CONNECTIONG_STRING)
+db = client.get_database('greendeck')
+collection = pymongo.collection.Collection(db, 'greendeck')
 
-api.add_resource(HelloWorld, '/')
+class Test(Resource):
+    def get(self):
+        print(collection.find_one())
+        return "Collecyion data read succesfully"
+
+api.add_resource(Test, '/')
 
 if __name__ == '__main__':
     application.run()
